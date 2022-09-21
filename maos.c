176527df55ef633cea40c12c94433419ce959461
PabloChristianno <pablochristianno@gmail.com>
2022-09-21 18:25:44 -0300

    Implementando estrutura de lista de adjacência.
    Signed-off-by: PabloChristianno <pablochristianno@gmail.com>

--- maos.c
+++ maos.c
@@ -5,8 +5,51 @@
     A -> Número de apertos
 */
 
+//Grafo
+struct graph {
+   int V; 
+   int A; 
+   link *adj; 
+};
+//Ponteiro para o grafo
+typedef struct graph *Graph;
+
+//lista de adjacência
+typedef struct node *link;
+struct node { 
+   int w; 
+   link next; 
+};
+
+//Função que adiciona nó na lista adjacência
+static link NEWnode( int w, link next) { 
+   link a = malloc( sizeof (struct node));
+   a->w = w; 
+   a->next = next;     
+   return a;                         
+}
+
+//inicializa um grafo
+Graph GRAPHinit( int V) { 
+   Graph G = malloc( sizeof *G);
+   G->V = V; 
+   G->A = 0;
+   G->adj = malloc( V * sizeof (link));
+   for (int v = 0; v < V; ++v) 
+      G->adj[v] = NULL;
+   return G;
+}
+
+//insere aresta no grafo (entender melhor)
+void GRAPHinsertArc( Graph G, int v, int w) { 
+   for (link a = G->adj[v]; a != NULL; a = a->next) 
+      if (a->w == w) return;
+   G->adj[v] = NEWnode( w, G->adj[v]);
+   G->A++;
+}
+
 int main(void){
-    int n, a;
+    int n, a, qntV, qntA;
     scanf("%d %d", &n, &a);
 
     int nomeOrigem[50], nomeDestino[50];
@@ -14,9 +57,11 @@ int main(void){
     for (int i = 0; i < n; i++)
     {
         // Ler e inserir Vertice
+        qntV = i+1;
     }
     for (int i = 0; i < a; i++){
         // Ler e inserir aresta
+        qntA = i+1;
     }
     
     // Buscar
