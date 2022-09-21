61f291ee067a04b039e5440ed4cf38330efbda7c
Abraao1231 <abraao1231@gmail.com>
2022-09-21 10:53:44 -0300

    add

---
+++ maos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    N -> Número de pessoas
+    A -> Número de apertos
+*/
+
+int main(void){
+    int n, a;
+    scanf("%d %d", &n, &a);
+
+    int nomeOrigem[50], nomeDestino[50];
+    scanf("%s %s", nomeOrigem, nomeDestino);
+    for (int i = 0; i < n; i++)
+    {
+        // Ler e inserir Vertice
+    }
+    for (int i = 0; i < a; i++){
+        // Ler e inserir aresta
+    }
+    
+    // Buscar
+
+    return 0;
+}
